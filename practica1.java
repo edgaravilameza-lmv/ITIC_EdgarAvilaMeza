728a8f015ae8ab56db17ad735bdeb8f3696ab92f
edgaravilameza-lmv <edgar.avila.meza@gmail.com>
2025-12-11 09:45:27 -0600

    Add files via upload

---
+++ practica1.java
@@ -0,0 +1,24 @@
+package Unidad5;
+import java.util.Scanner;
+public class practica1 {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        int edad;
+        do {
+            //do+tab
+            System.out.println("Ingresa tu edad");
+        edad=leer.nextInt();
+        } while (edad<0||edad>120);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
