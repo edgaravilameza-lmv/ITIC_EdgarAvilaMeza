795586038d9f4dbae1901550c46d3220503206e0
edgaravilameza-lmv <edgar.avila.meza@gmail.com>
2026-03-10 22:56:48 -0600

    Add files via upload

--- Calculadora.java
+++ Calculadora.java
@@ -1,40 +1,50 @@
 
-package Parcial2.Practica3;
+package Parcial2.Practica5;
 
 import java.util.Scanner;
 
-
 public class Calculadora {
- 
     public static void main(String[] args) {
- Scanner leer= new Scanner(System.in);
- double t;
-      t = leer.nextDouble();
-      Clases C = new Clases(t);
+        Scanner leer = new Scanner(System.in);
+int a, b;
+System.out.println("Dame dos numeros");       
+a=leer.nextInt();
+b=leer.nextInt();
+Matematicas mat = new Matematicas(a,b);
+
  int o=0;
-while(o!=4){
-        System.out.println("::::::::::MENU CALCULADORA::::::::::");
-        System.out.println("1.- Alta Temperatura en °Centigrados");
-                System.out.println("2.- Conversión de °Centigrados a °Farenheit");
-                System.out.println("3.- Conversión de °Centigrados a °Kelvin");
-                System.out.println("4.-Salir");
- o = leer.nextInt();
-Clases cal = new Clases(o);
-switch(o){
-    case 1:
-          System.out.println(t);
-          break;
-    case 2:
-       t= cal.Farenheit(t);
-      break;
-    case 3:
-        t= cal.Farenheit(t);
-  break;
-    case 4:
-        System.out.println(":::Saliendo:::");
-        break; 
-}
-    System.out.println(t);
+    while(o!=5){
+        System.out.println("1.-Suma");
+        System.out.println("2.-Resta");
+        System.out.println("3.-Divicion");
+        System.out.println("4.-Multiplicacion");
+        System.out.println("5.-Salir");
+    o=leer.nextInt();
+        switch(o){
+        case 1:
+            mat.suma();
+            System.out.println(mat.getR());     
+    break;
+        case 2:
+            mat.resta();
+            System.out.println(mat.getR());
+    break;
+        case 3:
+            mat.Division();
+            System.out.println(mat.getR());
+        break;
+        
+        case 4:
+            mat.Multiplicacion();
+            System.out.println(mat.getR());
+            break;
+        case 5:
+            System.out.println("::::::Saliendo::::::");
+            break;
+        }
+    
+    
     }
     }
+    
 }
--- Matematicas.java
+++ Matematicas.java
@@ -1,80 +1,72 @@
 
-package Parcial2.Practica1;
-
-   
+package Parcial2.Practica5;
 
 public class Matematicas {
-public void calcularFactorial(int numero ) {
-    int f =1 ;
-    for (int i = 1; i <= numero; i++) {
-        f=f*i;
+   int a, b;
+double r;
+    public Matematicas(int a, int b) {
+        this.a = a;
+        this.b = b;
     }
-    System.out.println("total factorial= "+f);
-}   
-public String esPar (int numero){
-int N=numero;
-if (N%2==0){
-    return "Es par";
-}else {
-    return "Es impar";
-}  
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public Matematicas(double r) {
+        this.r = r;
+    }
+
+    public double getR() {
+        return r;
+    }
+
+    public void setR(double r) {
+        this.r = r;
+    }
+   
+    
+    
+    private void su (){
+    r=this.getA()+this.getB();
+        this.setR(r);
+    }
+    public void suma(){
+    su();
+    }
+    
+    private void re(){
+    r=this.getA()-this.getB();
+    this.setR(r);
+    }
+    public void resta(){
+    re();
+    }
+    private void mu(){
+    r=this.getA()*this.getB();
+    this.setR(r);
+    }
+    public void Multiplicacion(){
+    mu();
+    }
+    private void di(){
+    r=this.getA()/this.getB();
+    this.setR(r);
+    }
+    public void Division(){
+    di();
+    }
+    
 }
-public int sumaDigitos (String numero){
-    String N= numero ;
-    int A=0, B=1;    
-    int K=0;
-    for (int i = 0; i < N.length(); i++) {
-              String D= N.substring(A, B);
-              A=A+1;
-              B=B+1;
-             
-           int d = Integer.parseInt(D);
-              K = K+d;
-    }
-    return K;
-    
-}
-public String esPrimo (int n){
-    
-    if (n<=1) {
-      return "no es primo";  
-    }
-    for (int i = 2; i < n; i++) {
-        if (n%i==0) {
-            return "no es primo";
-        }
-    }
-    
-            return "es primo";
-}
-public String invertirNumero (String n ){
-    int a=n.length()-1, b=n.length();
-    String CF="";
-    for (int i = 0; i < n.length(); i++) {
-        String subcadena = n.substring(a, b);
-        CF= CF+subcadena;
-        a=a-1;
-        b=b-1;
-    }
-    return CF;
-}
-public double Promedio (double n, double b,double a){
-    double r=0;
-    r=(n+b+a)/3;
-    
-    return r;
-}}
-
-
-
-
-  
-            
-  
-    
-    
-
-
-
-
-
